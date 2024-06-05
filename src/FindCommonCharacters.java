ac4cb7c788f6715889529c1d78f7f563c2f4280c
avishdhirawat <avishdhirawat0@gmail.com>
2024-06-05 23:49:24 +0530

    FindCommonCharacters: Bit Manipulation Question

---
+++ src/FindCommonCharacters.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class FindCommonCharacters {
+    public static List<String> commonChars(String[] words) {
+        int[] minFreq = new int[26];
+        Arrays.fill(minFreq, Integer.MAX_VALUE);
+
+        for (String word : words) {
+            int[] freq = new int[26];
+            for (char c : word.toCharArray()) {
+                freq[c - 'a']++;
+            }
+            for (int i = 0; i < 26; i++) {
+                minFreq[i] = Math.min(minFreq[i], freq[i]);
+            }
+        }
+
+        List<String> result = new ArrayList<>();
+        for (int i = 0; i < 26; i++) {
+            for (int j = 0; j < minFreq[i]; j++) {
+                result.add(String.valueOf((char) (i + 'a')));
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String[] words1 = {"bella", "label", "roller"};
+        System.out.println(commonChars(words1));
+
+        String[] words2 = {"cool", "lock", "cook"};
+        System.out.println(commonChars(words2));
+    }
+}
