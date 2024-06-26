6b9781e9070f31c489adfadc01c87df63e89f2c7
avishdhirawat <avishdhirawat0@gmail.com>
2024-06-26 21:41:58 +0530

    ThreeSum Problem: Array Manipulation Question (Medium-Hard)

---
+++ src/ThreeSum.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class ThreeSum {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) {
+            return result;
+        }
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+
+                if (sum == 0) {
+                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;
+                    right--;
+
+                    while (left < right && nums[left] == nums[left - 1]) {
+                        left++;
+                    }
+
+                    while (left < right && nums[right] == nums[right + 1]) {
+                        right--;
+                    }
+                } else if (sum < 0) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        ThreeSum solution = new ThreeSum();
+
+        int[] nums1 = {-1, 0, 1, 2, -1, -4};
+        System.out.println(solution.threeSum(nums1));
+        int[] nums2 = {0, 1, 1};
+        System.out.println(solution.threeSum(nums2));
+        int[] nums3 = {0, 0, 0};
+        System.out.println(solution.threeSum(nums3));
+
+    }
+}
